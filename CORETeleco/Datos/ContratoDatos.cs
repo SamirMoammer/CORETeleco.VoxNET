6b76635ddf59c0afa386d57caf9f186535a0a26a
Samir Moammer <1106232@est.intec.edu.do>
2024-07-07 22:34:20 -0400

    updated

--- CORETeleco/Datos/ContratoDatos.cs
+++ CORETeleco/Datos/ContratoDatos.cs
@@ -1,6 +1,8 @@
 ﻿using CORETeleco.Models;
-using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace CORETeleco.Datos
 {
--- CORETeleco/Models/ContratoModel.cs
+++ CORETeleco/Models/ContratoModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CORETeleco.Models
 {
@@ -7,23 +8,25 @@ namespace CORETeleco.Models
         public int idContrato { get; set; }
 
         [Required(ErrorMessage = "El campo Fecha de Inicio es obligatorio")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Inicio")]
         public DateTime fechaInicioContrato { get; set; }
 
         [Required(ErrorMessage = "El campo Fecha de Fin es obligatorio")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Fin")]
         public DateTime fechaFinContrato { get; set; }
 
         [Required(ErrorMessage = "El campo Descripción es obligatorio")]
-        public string? descripcionContrato { get; set; }
+        public string descripcionContrato { get; set; }
 
         [Required(ErrorMessage = "El campo Estado es obligatorio")]
         public bool estadoContrato { get; set; }
 
         [Required(ErrorMessage = "El campo Cliente es obligatorio")]
+        [Display(Name = "Cliente")]
         public int idCliente { get; set; }
 
         [Required(ErrorMessage = "El campo Servicio es obligatorio")]
+        [Display(Name = "Servicio")]
         public int idServicio { get; set; }
     }
 }
